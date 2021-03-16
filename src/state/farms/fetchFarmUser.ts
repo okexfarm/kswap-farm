9b3f73e14fcff0bab221f59e8a3d97aa1cbcdc53
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-17 01:05:02 +0530

    Update fetchFarmUser.ts

--- src/state/farms/fetchFarmUser.ts
+++ src/state/farms/fetchFarmUser.ts
@@ -63,7 +63,7 @@ export const fetchFarmUserEarnings = async (account: string) => {
   const calls = farmsConfig.map((farm) => {
     return {
       address: masterChefAdress,
-      name: 'pendingMango',
+      name: 'pendingKswap',
       params: [farm.pid, account],
     }
   })
