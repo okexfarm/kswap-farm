006dcd9cfcf009f2e0544e242dc914b166af8322
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-17 00:51:17 +0530

    Update types.ts

--- src/state/types.ts
+++ src/state/types.ts
@@ -8,7 +8,7 @@ export interface Farm extends FarmConfig {
   tokenPriceVsQuote?: BigNumber
   poolWeight?: number
   depositFeeBP?: number
-  mangoPerBlock?: number
+  kswapPerBlock?: number
   userData?: {
     allowance: BigNumber
     tokenBalance: BigNumber
