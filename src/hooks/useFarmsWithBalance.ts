351c660651a1e66ebc584d677199b8d25c8d12e9
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-17 01:20:11 +0530

    Update useFarmsWithBalance.ts

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingMango',
+        name: 'pendingKswap',
         params: [farm.pid, account],
       }))
 
