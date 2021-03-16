e13858326815c304ed5ec21299a3ea3ab460a9e0
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-17 01:08:11 +0530

    Update useUnstake.ts

--- src/hooks/useUnstake.ts
+++ src/hooks/useUnstake.ts
@@ -7,8 +7,8 @@ import {
   updateUserBalance,
   updateUserPendingReward,
 } from 'state/actions'
-import { unstake, mangoUnstake, mangoEmegencyUnstake } from 'utils/callHelpers'
-import { useMasterchef, useMangoChef } from './useContract'
+import { unstake, kswapUnstake, kswapEmegencyUnstake } from 'utils/callHelpers'
+import { useMasterchef, useKswapChef } from './useContract'
 
 const useUnstake = (pid: number) => {
   const dispatch = useDispatch()
@@ -33,7 +33,7 @@ export const useMangoUnstake = (juiceId) => {
   const dispatch = useDispatch()
   const { account } = useWallet()
   const masterChefContract = useMasterchef()
-  const mangoChefContract = useMangoChef(juiceId)
+  const kswapChefContract = useKswapChef(juiceId)
   const isOldJuice = JUICEIDS.includes(juiceId)
 
   const handleUnstake = useCallback(
@@ -42,17 +42,17 @@ export const useMangoUnstake = (juiceId) => {
         const txHash = await unstake(masterChefContract, 0, amount, account)
         console.info(txHash)
       } else if (isOldJuice) {
-        const txHash = await mangoEmegencyUnstake(mangoChefContract, amount, account)
+        const txHash = await kswapEmegencyUnstake(kswapChefContract, amount, account)
         console.info(txHash)
       } else {
-        const txHash = await mangoUnstake(mangoChefContract, amount, account)
+        const txHash = await kswapUnstake(kswapChefContract, amount, account)
         console.info(txHash)
       }
       dispatch(updateUserStakedBalance(juiceId, account))
       dispatch(updateUserBalance(juiceId, account))
       dispatch(updateUserPendingReward(juiceId, account))
     },
-    [account, dispatch, isOldJuice, masterChefContract, mangoChefContract, juiceId],
+    [account, dispatch, isOldJuice, masterChefContract, kswapChefContract, juiceId],
   )
 
   return { onUnstake: handleUnstake }
