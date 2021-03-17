cfa84e0900e4831b4804f2e389286e66a970f2cb
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-17 11:53:20 +0530

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -3,15 +3,15 @@ import { PoolConfig, QuoteToken, PoolCategory } from './types'
 const pools: PoolConfig[] = [
 /*  {
   juiceId: 0,
- tokenName: 'MANGO',
-  stakingTokenName: QuoteToken.MANGO,
+ tokenName: 'KSWAP',
+  stakingTokenName: QuoteToken.KSWAP,
 stakingTokenAddress: '0x3D29cdB00CB183a0aEcEEa4fB73F55E1450af3d4',
  contractAddress: {
    97: '',
    56: '0xB8c0B207a2eC0FB7011FAF2F5d7Fb98D2222fF98',
      },
     poolCategory: PoolCategory.CORE,
-   projectLink: 'https://mangofarm.finance/',
+   projectLink: 'https://bsc.kswap.net/',
     harvest: true,
   tokenPerBlock: '0.5',
    sortOrder: 1,
