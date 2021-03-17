dbcda830980673c073296c14e52b523f84d71a7e
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-17 14:45:12 +0530

    Update HowItWorks.tsx

--- src/views/Lottery/components/HowItWorks.tsx
+++ src/views/Lottery/components/HowItWorks.tsx
@@ -35,7 +35,7 @@ const HowItWorks = () => {
       <Text fontSize="16px">
         {TranslateString(
           999,
-          'Spend MANGO to buy tickets, contributing to the lottery pot. Win prizes if 2, 3, or 4 of your ticket numbers match the winning numbers and their exact order!',
+          'Spend KSWAP to buy tickets, contributing to the lottery pot. Win prizes if 2, 3, or 4 of your ticket numbers match the winning numbers and their exact order!',
         )}
       </Text>
       <StyledLink href="https://docs.mangoswap.finance/lottery-1">Read more</StyledLink>
