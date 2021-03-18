f87278a40ca7aeb57544f0e86782786e3e27243b
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-18 15:02:13 +0530

    Update api.ts

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 /*
  * Due to Cors the api was forked and a proxy was created
- * @see https://github.com/mangoswap/gatsby-mango-api/commit/e811b67a43ccc41edd4a0fa1ee704b2f510aa0ba
+ * @see https://github.com/kswap/gatsby-kswap-api/commit/e811b67a43ccc41edd4a0fa1ee704b2f510aa0ba
  */
 export const baseUrl = 'https://pancakeswap.info/'
 
