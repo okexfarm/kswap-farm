4fca1a9691140712a6b9018194f9fbbd7bc68662
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-18 22:33:20 +0530

    Update PastRoundCardDetails.tsx

--- src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCardDetails.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCardDetails.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Heading, CardBody, CardFooter, MangoRoundIcon, TicketRound } from '@mangofarm/uikit'
+import { Heading, CardBody, CardFooter, KswapRoundIcon, TicketRound } from '@mangofarm/uikit'
 import useI18n from 'hooks/useI18n'
 import { DataResponse } from 'utils/getLotteryRoundData'
 import LotteryCardHeading from '../LotteryCardHeading'
