c4249cf10bdcf9fcdc3e8472db165962128a581a
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-18 23:17:55 +0530

    Update compoundApyHelpers.ts

--- src/utils/compoundApyHelpers.ts
+++ src/utils/compoundApyHelpers.ts
@@ -7,7 +7,7 @@ export const calculateKswapEarnedPerThousandDollars = ({ numberOfDays, farmApy, 
   const apyAsDecimal = farmApy / 100
   const daysAsDecimalOfYear = numberOfDays / timesCompounded
   //   Calculate the starting KSWAP balance with a dollar balance of $1000.
-  const principal = 1000 / mangoPrice
+  const principal = 1000 / kswapPrice
 
   // This is a translation of the typical mathematical compounding APY formula. Details here: https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php
   const finalAmount = principal * (1 + apyAsDecimal / timesCompounded) ** (timesCompounded * daysAsDecimalOfYear)
