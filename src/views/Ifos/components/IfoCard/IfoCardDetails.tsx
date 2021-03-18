54f63476ac5fefd16a13f189c4791f91b785fe18
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-18 22:45:56 +0530

    Update IfoCardDetails.tsx

--- src/views/Ifos/components/IfoCard/IfoCardDetails.tsx
+++ src/views/Ifos/components/IfoCard/IfoCardDetails.tsx
@@ -9,7 +9,7 @@ export interface IfoCardDetailsProps {
   launchTime: string
   saleAmount: string
   raiseAmount: string
-  mangoToBurn: string
+  kswapToBurn: string
   projectSiteUrl: string
   raisingAmount: BigNumber
   totalAmount: BigNumber
@@ -68,8 +68,8 @@ const IfoCardDetails: React.FC<IfoCardDetailsProps> = ({
           <Text>{raiseAmount}</Text>
         </Item>
         <Item>
-          <Display>{TranslateString(586, 'MANGO to burn (USD)')}</Display>
-          <Text>{mangoToBurn}</Text>
+          <Display>{TranslateString(586, 'KSWAP to burn (USD)')}</Display>
+          <Text>{kswapToBurn}</Text>
         </Item>
         <Item>
           <Display>{TranslateString(999, 'Total raised (% of target)')}</Display>
