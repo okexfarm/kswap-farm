b001517a49bea26e8fb741741f3b96a49e43f59f
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-18 21:26:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App: React.FC = () => {
             <Route path="/farms">
               <Farms />
             </Route>
-            <Route path="/mango">
+            <Route path="/kswap">
               <Farms tokenMode />
             </Route>
             {/* <Route path="/pools"> */}
