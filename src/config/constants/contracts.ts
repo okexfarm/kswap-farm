6ca705c054675f31b8f6fd57f521bce53cbc6007
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-19 14:04:53 +0530

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,9 +1,9 @@
 export default {
   kswap: {
-    56: '0x3D29cdB00CB183a0aEcEEa4fB73F55E1450af3d4',
+    56: '0xFd53F26Cb14aAa4AA90a32B7e9bd9B12D3D42Ec7',
   },
   masterChef: {
-    56: '0xB8c0B207a2eC0FB7011FAF2F5d7Fb98D2222fF98',
+    56: '0x59B58b7B249838f498d56f9D7FFEe16E32b9E7C0',
   },
   wbnb: {
     56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
